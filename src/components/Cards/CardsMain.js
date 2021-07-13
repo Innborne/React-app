2a9697dd6b78e87848d28ef083ca5691867474cd
Ilya Navumau <innborne@gmail.com>
2021-07-13 16:41:39 +0300

    added CardsCheckbox component -used styled-components for CardsCheckbox -read-only mode checkbox switched to CardsCheckbox component

--- src/components/Cards/CardsMain.js
+++ src/components/Cards/CardsMain.js
@@ -1,21 +1,8 @@
 import { useState } from "react";
+import CardsCheckbox from "./CardsCheckbox";
 import CardsItem from "./CardsItem";
-import styled from "styled-components";
+
 import "./CardsMain.css";
-
-const CheckboxContainer = styled.div`
-  display: flex;
-  width: 100%;
-  margin-left: 0.25rem;
-  align-items: center;
-  color: white;
-  opacity: ${(props) => (props.passed ? "100%" : "50%")};
-
-  & input {
-    margin: 0.5rem;
-    transform: scale(1.6);
-  }
-`;
 
 function CardsMain(props) {
   const [readOnlyMode, setReadOnlyMode] = useState(true);
@@ -30,14 +17,13 @@ function CardsMain(props) {
 
   return (
     <div className="cards-main">
-      <CheckboxContainer passed={readOnlyMode}>
-        <input
-          type="checkbox"
-          checked={readOnlyMode}
-          onChange={readOnlyHandleClick}
-        ></input>
-        <label>Read only</label>
-      </CheckboxContainer>
+      <CardsCheckbox
+        type="checkbox"
+        checked={readOnlyMode}
+        onChange={readOnlyHandleClick}
+      >
+        Read only
+      </CardsCheckbox>
       {props.items.map((cardData) => (
         <CardsItem
           onSaveCardData={saveCardDataHandler}
