086c639f680abc3d58de8ae69d01e98e4c4c7069
Ilya Navumau <innborne@gmail.com>
2021-08-02 22:29:52 +0300

    added sign in page -added 404 page, if page not found -used react-router-dom for routing between pages -added navigation links to header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import CardList from './components/CardList/CardList';
 import { CardListContextProvider } from './components/CardListContext/CardListContext';
 import Header from './components/Header/Header';
+import NotFound from './components/NotFound/NotFound';
+import { AuthWithDelay } from './components/Auth/Auth';
 
 function App() {
-  const header = 'Header';
-
   return (
-    <CardListContextProvider>
-      <Header>{header}</Header>
-      <CardList />
-    </CardListContextProvider>
+    <BrowserRouter>
+      <CardListContextProvider>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <CardList />
+          </Route>
+          <Route path="/notfound" exact>
+            <NotFound />
+          </Route>
+          <Route path="/auth" exact>
+            <AuthWithDelay />
+          </Route>
+          <Route path="*">
+            <Redirect to="/notfound" />
+          </Route>
+        </Switch>
+      </CardListContextProvider>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,25 @@
 import { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 import CardListContext from '../CardListContext/CardListContext';
 
 import './Header.css';
 
-function Header(props) {
+function Header() {
   const ctx = useContext(CardListContext);
-  const cardBadgeText = 'Cards';
+  const cardBadgeText = 'Cards',
+    homePageText = 'Home',
+    signInPageText = 'Sign in';
+
   return (
     <header className="header">
-      <div>{props.children}</div>
+      <div className="nav-container">
+        <NavLink to="/" activeClassName="active" exact>
+          {homePageText}
+        </NavLink>
+        <NavLink to="/auth" activeClassName="active">
+          {signInPageText}
+        </NavLink>
+      </div>
       <div className="header-text-badge">
         {cardBadgeText}
         <span className="header-badge">{ctx.items.length}</span>
