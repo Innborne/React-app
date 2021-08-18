8b5328199106c52f1de4187f3bffca7258223e8e
Ilya Navumau <innborne@gmail.com>
2021-08-18 19:50:53 +0300

    used thunk and logger when dispatching any action

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import cardListReducer from './cards';
 
-const store = configureStore({ reducer: cardListReducer });
+const logger = () => (next) => (action) => {
+  console.log('dispatching', action);
+  return next(action);
+};
+
+const store = configureStore({
+  reducer: cardListReducer,
+  middleware: [thunk, logger],
+});
 
 export default store;
